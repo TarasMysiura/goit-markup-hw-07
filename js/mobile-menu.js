16731af24a277e86d0d6558cae9b1e6d9ed907c8
TarasMysiura <118620052+TarasMysiura@users.noreply.github.com>
2023-01-31 00:59:12 +0200

    Change 2

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,12 +2,13 @@
   const mobileMenu = document.querySelector(".js-menu-container");
   const openMenuBtn = document.querySelector(".js-open-menu");
   const closeMenuBtn = document.querySelector(".js-close-menu");
+  const bodyNoScroll = document.querySelector("body")
 
   const toggleMenu = () => {
     const isMenuOpen =
       openMenuBtn.getAttribute("aria-expanded") === "true" || false;
     openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
-    mobileMenu.classList.toggle("is-open");
+    mobileMenu.classList.toggle("is-hidden");
 
     const scrollLockMethod = !isMenuOpen
       ? "disableBodyScroll"
@@ -21,7 +22,7 @@
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
     if (!e.matches) return;
-    mobileMenu.classList.remove("is-open");
+    mobileMenu.classList.remove("is-hidden");
     openMenuBtn.setAttribute("aria-expanded", false);
     bodyScrollLock.enableBodyScroll(document.body);
   });
